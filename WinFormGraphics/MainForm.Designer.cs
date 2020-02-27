cf70d91908051c0e753e3857ac08e8d493cd9b09
Emiel Nijhuis <enijhuis@outlook.com>
2020-02-27 15:11:06 +0100

    add the actual project

---
+++ WinFormGraphics/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WinFormGraphics
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // label1
+            this.label1 = new System.Windows.Forms.Label();
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TODO : label + input";
+            this.Controls.Add(this.label1);
+
+            // button1
+            this.button1 = new System.Windows.Forms.Button();
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(170, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 22);
+            this.button1.Text = "click me!";
+            //Raises the click event of the button
+            this.button1.Click += Button1_Click;
+            this.Controls.Add(this.button1);
+
+            // groupBox1            
+            this.groupBox1 = new System.Windows.Forms.GroupBox();            
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            //this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(18, 112);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(700, 466);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size 200 etc.";
+            //Raises the paint event of the button
+            this.groupBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBox1_Paint);
+
+            
+            // MainForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 590);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "CSWinFormGraphics";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ WinFormGraphics/MainForm.cs
@@ -0,0 +1,116 @@
+﻿/************************************* Module Header **************************************\
+* Module Name:  MainForm.cs
+* Project:      WinFormGraphics
+* Copyright (c) Microsoft Corporation.
+* 
+* The Graphics sample demonstrates the fundamentals of GDI+ programming. 
+* GDI+ allows you to create graphics, draw text, and manipulate graphical images as objects. 
+* GDI+ is designed to offer performance as well as ease of use. 
+* You can use GDI+ to render graphical images on Windows Forms and controls. 
+* GDI+ has fully replaced GDI, and is now the only way to render graphics programmatically 
+* in Windows Forms applications.
+*
+* In this sample, there're 5 examples:
+* 
+* 1. Draw A Line.
+*    Demonstrates how to draw a solid/dash/dot line.
+* 2. Draw A Curve.
+*    Demonstrates how to draw a curve, and the difference between antialiasing rendering mode
+*    and no antialiasing rendering mode.
+* 3. Draw An Arrow.
+*    Demonstrates how to draw an arrow.
+* 4. Draw A Vertical String.
+*    Demonstrates how to draw a vertical string.
+* 5. Draw A Ellipse With Gradient Brush.
+*    Demonstrates how to draw a shape with gradient effect.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+* ***************************************************************************/
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace WinFormGraphics
+{
+    public partial class MainForm : Form
+    {
+        private IEnumerable<Point> Points { get; set; }
+
+        public MainForm()
+        {
+            Points = new List<Point>(){
+                    new Point(40,250),
+                    new Point(80,300),
+                    new Point(120,200)};
+
+            InitializeComponent();
+        }
+
+        //Perform any action on click action method
+        private void Button1_Click(object sender, EventArgs e)
+        {            
+            MessageBox.Show("Button was clicked");
+            Refresh();
+        }
+
+        public override void Refresh()
+        {
+            Points = new List<Point>(){
+                    new Point(40,40),
+                    new Point(80,80),
+                    new Point(120,120)};
+
+            base.Refresh();
+        }
+
+        private void groupBox1_Paint(object sender, PaintEventArgs e)
+        {            
+            DrawEllipseRectangles(e, Points);
+        }
+
+        private void DrawEllipseRectangles(PaintEventArgs e, IEnumerable<Point> points)
+        {
+            using (var pen = new Pen(Color.Black, 1))
+            {
+                points.ToList().ForEach(p => DrawEllipseRectangle(e, pen, p));
+            }
+        }
+
+        private void DrawEllipseRectangle(PaintEventArgs e, Pen pen, Point point)
+        {
+
+            // Create rectangle for ellipse.
+            Rectangle rect = new Rectangle(point, new Size(4, 4));
+
+            // Draw ellipse to screen.
+            e.Graphics.DrawEllipse(pen, rect);
+        }
+
+
+
+        private void ObsoleteMethod(PaintEventArgs e)
+        {
+            using (var p = new Pen(Color.Black))
+            {
+                // Specify a collection of points for the curve.
+                var points = new Point[]{
+                    new Point(40,250),
+                    new Point(80,300),
+                    new Point(120,200)};
+
+                e.Graphics.DrawCurve(p, points);
+
+                e.Graphics.DrawString(
+                        "This is a horizontal text.",
+                        this.Font, new SolidBrush(Color.Red), 450, 90, new StringFormat());
+
+            }
+        }
+    }
+}
